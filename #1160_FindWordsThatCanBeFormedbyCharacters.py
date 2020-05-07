470057853d9705c3352f6e275c82b382c26f4058
Mia <ikuyuhsin@gmail.com>
2020-05-07 11:45:23 -0700

    Add solution for question 1160.

---
+++ #1160_FindWordsThatCanBeFormedbyCharacters.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countCharacters(self, words: [str], chars: str) -> int:
+        sum = 0
+        
+        for word in words:
+            chars_list = list(chars)
+            for char in word:
+                if char in chars_list:
+                    chars_list.remove(char)
+                else:
+                    break
+            else:
+                sum += len(word)
+        return sum
+                
+
+words = ["cat", "bt", "hat", "tree"]
+chars = "atach"
+
+s = Solution()
+print(s.countCharacters(words, chars))
