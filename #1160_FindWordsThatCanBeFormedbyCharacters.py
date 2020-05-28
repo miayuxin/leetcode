503ac0e4bdecc8c59b9a011379daa7a52b306ceb
Mia <ikuyuhsin@gmail.com>
2020-05-28 15:34:18 -0700

    Add 2 solutions for question 160 with 2 different space complexities.

--- #1160_FindWordsThatCanBeFormedbyCharacters.py
+++ #1160_FindWordsThatCanBeFormedbyCharacters.py
@@ -26,3 +26,11 @@ chars = "atach"
 s = Solution()
 print(s.wordCanBeFormed(words, chars))
 print(s.countCharacters(words, chars))
+
+# 大目標是countCharacters，if word can be formed by chars, 就返回字母數的sum.
+# 小目標是wordCanBeFormed, 把word的每個字母拿出來跟chars_list對照，確認是否在chars_list裡面。
+# 第一次跑時：
+# chars_list = ["a", "t", "a", "c", "h"]
+# word = "cat", char = "c" 
+# 所以 "c" 在 chars_list 裡，就把chars_list的"c"拿掉，因為不能重複比對，剩下 chars_list = ["a", "t", "a", "h"]
+# 剩下以此類推，直到"cat"結束後，會是成立的所以 return True
