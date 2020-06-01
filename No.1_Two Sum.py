32c98b1f9fa3b59654b43d2015e429c9a578ec59
Mia <ikuyuhsin@gmail.com>
2020-06-01 12:23:31 -0700

    Add the notes to interpret my code.

--- No.1_Two Sum.py
+++ No.1_Two Sum.py
@@ -7,9 +7,14 @@ class Solution:
         
         for i in range(len(nums)):
             temp = target - nums[i]
+            
+            # Can't use the same elements twice, e.g., [3, 2, 4], target = 6, 
+            # the expected output should be [1, 2], instead of [0, 0].
+            # So set the additional condition num_dict[temp] != i as below. 
+
             if temp in num_dict and num_dict[temp] != i:
                 return i, num_dict[temp]
 
 
 s = Solution()
-print(s.twoSum([2,7,11,15],9))
+print(s.twoSum([2,7,11,15], 9))
